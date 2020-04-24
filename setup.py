19b1ddc575d557e3b54b8411af43c91fd392502e
Juan Gamella <juangamella@gmail.com>
2020-04-24 14:22:31 +0200

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='sempler',
+    version='0.1dev',
+    packages=['sempler',],
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    long_description=open('README.txt').read(),
+)
