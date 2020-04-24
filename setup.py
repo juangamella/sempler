d05b3036b9fc072ee18c8089c29849bf17effc72
Juan Gamella <juangamella@gmail.com>
2020-04-24 14:32:09 +0200

    Package setup

--- setup.py
+++ setup.py
@@ -2,8 +2,15 @@ from distutils.core import setup
 
 setup(
     name='sempler',
-    version='0.1dev',
-    packages=['sempler',],
-    license='BSD 3-Clause License',
+    version='0.0.1',
+    author='Juan Luis Gamella',
+    author_email='juangamella@gmail.com',
+    packages=['sempler', 'sempler.test'],
+    scripts=['bin/stowe-towels.py','bin/wash-towels.py'],
+    url='http://pypi.python.org/pypi/sempler/',
+    license='LICENSE.txt',
+    description='Sample from arbitrary structural equation models (SEMs)',
     long_description=open('README.txt').read(),
+    install_requires=[
+    ],
 )
