709cfe730a1c7ea079ad45f9e3892f7d34139d4c
Juan Gamella <juangamella@gmail.com>
2020-04-30 19:38:15 +0200

    Added explicit versions to the required packages

--- setup.py
+++ setup.py
@@ -11,5 +11,5 @@ setup(
     license='LICENSE.txt',
     description='Sample from general structural causal models (SCMs)',
     long_description=open('README.txt').read(),
-    install_requires=['numpy', 'networkx', 'matplotlib']
+    install_requires=['numpy==1.18.3', 'networkx==2.2', 'matplotlib==3.2.1']
 )
