c6ca7ce42f79b903dcfb130abe0457bb81ac8ed4
Juan Gamella <juangamella@gmail.com>
2021-01-31 15:39:13 +0100

    Updated changelog and version number2

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ import setuptools
 
 setuptools.setup(
     name='sempler',
-    version='0.1.4',
+    version='0.2.0',
     author='Juan L Gamella',
     author_email='juangamella@gmail.com',
     packages=['sempler', 'sempler.test'],
     scripts=[],
     url='https://github.com/juangamella/sempler',
     license='BSD 3-Clause License',
-    description='Sample from general structural causal models (SCMs)',
+    description='Sample from general structural causal models (SCMs).',
     long_description=open('README.md').read(),
     long_description_content_type="text/markdown",
     install_requires=['numpy>=1.15.0', 'matplotlib', 'networkx>=2.2']
