49f2c20c89b0b3e7ba553853be1ecd620ba74cee
Juan L. Gamella <juan.gamella@stat.math.ethz.ch>
2022-06-22 10:28:03 +0200

    Expanded generators.intervention_targets function to allow sampling without replacement

--- sempler/generators.py
+++ sempler/generators.py
@@ -170,7 +170,7 @@ def dag_full(p, w_min=1, w_max=1, return_ordering=False, random_state=None):
         return W[permutation, :][:, permutation]
 
 
-def intervention_targets(p, K, size, random_state=None):
+def intervention_targets(p, K, size, replace=True, random_state=None):
     """Sample a set of intervention targets.
 
     Parameters
@@ -185,6 +185,10 @@ def intervention_targets(p, K, size, random_state=None):
         targets / intervention. If a two-element tuple, the number of
         targets is sampled uniformly at random from `[size[0],
         size[1]]`.
+    replace : bool, default=True
+        Wether the intervention targets should be sampled with
+        replacement, i.e. if repeated targets are allowed across
+        environments.
     random_state : int or None
         To set the random state for reproducibility.
 
@@ -207,19 +211,28 @@ def intervention_targets(p, K, size, random_state=None):
 
     >>> from sempler.generators import intervention_targets
     >>> intervention_targets(10, 5, 1, random_state=42)
-    [[8], [0], [9], [1], [1]]
+    [[0], [7], [6], [4], [4]]
+
+    Without replacement:
+    >>> intervention_targets(10, 5, 1, replace=False, random_state=42)
+    [[0], [7], [6], [4], [3]]
 
     Generating a set of interventions with random number of targets:
 
     >>> intervention_targets(10, 5, (1,3), random_state=42)
-    [[9, 7, 0], [0], [5, 2, 7], [1, 5, 4], [8]]
+    [[8], [2, 6, 0], [8, 7], [6, 7], [8, 1]]
+
+    Without replacement:
+
+    >>> intervention_targets(10, 5, (1,2), replace=False, random_state=42)
+    [[8], [6, 0], [1, 4], [7], [9]]
 
     An exception is raised if `size > p`:
 
     >>> intervention_targets(4, 5, 5)
     Traceback (most recent call last):
       ...
-    ValueError: The intervention size cannot be larger than the number of variables.
+    ValueError: The (max.) intervention size cannot be larger than the number of variables.
 
     Or if `size` is a tuple with size different than two:
 
@@ -228,26 +241,49 @@ def intervention_targets(p, K, size, random_state=None):
       ...
     ValueError: The intervention size must be a positive integer or two-element tuple.
 
+    If sampling targets without replacement, the maximum intervention
+    size and number of interventions must be set accordingly,
+    i.e. `max_size x K <= p`. Otherwise an exception is raised:
+    >>> intervention_targets(10, 5, (0,3), replace=False)
+    Traceback (most recent call last):
+      ...
+    ValueError: Cannot sample targets without replacement for the given intervention size and number of interventions.
+
+
     """
-    np.random.seed(random_state) if random_state is not None else None
+    rng = np.random.default_rng(random_state)
     # Build intervention sizes
     if isinstance(size, tuple) and len(size) == 2:
-        max_size = size[1]
-        sizes = np.random.randint(size[0], size[1] + 1, K)
+        min_size, max_size = size
+        sizes = rng.integers(size[0], size[1] + 1, K)
     elif isinstance(size, tuple):
         raise ValueError("The intervention size must be a positive integer or two-element tuple.")
     else:
         max_size = size
         sizes = [size] * K
+    # If sampling without
+    if not replace:
+        if max_size * K > p:
+            raise ValueError(
+                "Cannot sample targets without replacement for the given intervention size and number of interventions.")
     # Check max size condition
     if max_size > p:
-        raise ValueError("The intervention size cannot be larger than the number of variables.")
+        raise ValueError(
+            "The (max.) intervention size cannot be larger than the number of variables.")
     # Sample the targets
-    interventions = []
-    targets = list(range(p))
-    for i, k in enumerate(range(K)):
-        intervention = list(np.random.choice(list(targets), size=sizes[i], replace=False))
-        interventions.append(intervention)
+    if replace:
+        interventions = []
+        targets = list(range(p))
+        for i, k in enumerate(range(K)):
+            intervention = list(rng.choice(targets, size=sizes[i], replace=False))
+            interventions.append(intervention)
+    else:
+        interventions = []
+        remaining_targets = set(range(p))
+        for i, k in enumerate(range(K)):
+            intervention = list(rng.choice(list(remaining_targets), size=sizes[i], replace=False))
+            remaining_targets -= set(intervention)
+            interventions.append(intervention)
     return interventions
 
 
