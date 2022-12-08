14e926076e570a580787f916aec245c476a5bd8d
Juan Gamella Martin <juan.gamella@stat.math.ethz.ch>
2022-12-08 17:35:48 +0100

    Fixed bug

---
+++ docs/semi_example.py
@@ -0,0 +1,9 @@
+import sempler
+import sempler.generators
+import numpy as np
+
+rng = np.random.default_rng(42)
+data = [rng.uniform(size=(100, 5)) for _ in range(2)]
+graph = sempler.generators.dag_avg_deg(p=5, k=2, random_state=42)
+network = sempler.DRFNet(graph, data)
+print(network.graph)
--- sempler/semi.py
+++ sempler/semi.py
@@ -39,7 +39,7 @@ Please cite it if you use this procedure in your work.
 
 import numpy as np
 import drf
-import sempler.utils as utils
+import sempler.utils
 import copy
 import pandas as pd
 import time
