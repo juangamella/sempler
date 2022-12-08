c54f9e544ea63508a345058aeff2c4aea792cd30
Juan Gamella Martin <juan.gamella@stat.math.ethz.ch>
2022-12-08 17:39:46 +0100

    Adapted wording

--- sempler/semi.py
+++ sempler/semi.py
@@ -247,7 +247,7 @@ class DRFNet(BayesianNetwork):
         ) if verbose else None
 
     def sample(self, n=None, random_state=None):
-        """Generate a sample from the fitted SCM.
+        """Generate a sample from the fitted Bayesian network.
 
         Parameters
         ----------
