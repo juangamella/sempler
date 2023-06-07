8be1c6f93816c1dec591a5bf3cb67c178f6c5cf0
Juan Gamella Martin <juan.gamella@stat.math.ethz.ch>
2023-06-07 19:00:15 +0200

    Add drf as necessary package

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setuptools.setup(
     version="0.2.10",
     author="Juan L Gamella",
     author_email="juangamella@gmail.com",
-    packages=["sempler"],
+    packages=["sempler", "drf"],
     scripts=[],
     url="https://github.com/juangamella/sempler",
     license="BSD 3-Clause License",
