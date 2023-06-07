2201949aee1d19d05e5340d614e49256debbc20e
Juan Gamella Martin <juan.gamella@stat.math.ethz.ch>
2023-06-07 15:44:29 +0200

    Made R and rpy2 optional dependencies

--- sempler/__init__.py
+++ sempler/__init__.py
@@ -1,4 +1,11 @@
 from .normal_distribution import NormalDistribution
 from .anm import ANM
 from .lganm import LGANM
-from .semi import DRFNet
+
+try:
+    from .semi import DRFNet
+except Exception as e:
+    print(
+        "Did not load sempler.semi module and sempler.DRFNet class - optional dependencies are missing. See https://github.com/juangamella/sempler#installation for more details. All other functionality is available."
+    )
+    print(e)
--- setup.py
+++ setup.py
@@ -2,15 +2,16 @@ import setuptools
 
 setuptools.setup(
     name="sempler",
-    version="0.2.9",
+    version="0.2.10",
     author="Juan L Gamella",
     author_email="juangamella@gmail.com",
-    packages=["sempler", "sempler.test", "drf"],
+    packages=["sempler"],
     scripts=[],
     url="https://github.com/juangamella/sempler",
     license="BSD 3-Clause License",
     description="Sample from general structural causal models (SCMs).",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
-    install_requires=["numpy>=1.17.0", "pandas>=1.2.1", "rpy2>=3.4.1"],
+    install_requires=["numpy>=1.17.0", "pandas>=1.2.1"],
+    extras_require={"DRFNet": ["rpy2>=3.4.1"]},
 )
