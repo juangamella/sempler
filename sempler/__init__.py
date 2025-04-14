9d44ce1b8edefa7fc3db69ad912acf8c06d1ad14
Alex Markham <alex.markham@causal.dev>
2025-04-14 14:36:29 +0200

    refactor: explicitly catch import error and issue warning

--- sempler/__init__.py
+++ sempler/__init__.py
@@ -1,11 +1,12 @@
+import warnings
+
 from .normal_distribution import NormalDistribution
 from .anm import ANM
 from .lganm import LGANM
 
 try:
     from .semi import DRFNet
-except Exception as e:
-    print(
-        "Did not load sempler.semi module and sempler.DRFNet class - optional dependencies are missing. See https://github.com/juangamella/sempler#installation for more details. All other functionality is available."
+except ImportError as e:
+    warnings.warn(
+        f"{e}. Did not load sempler.semi module and sempler.DRFNet class - optional dependencies are missing. See https://github.com/juangamella/sempler#installation for more details. All other functionality is available."
     )
-    print(e)
